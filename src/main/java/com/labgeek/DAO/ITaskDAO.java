787629f7e21d66a1ab1ef3ca2c8b0b450eb4bfa0
elboraki <younes.elboraki@gmail.com>
2025-08-14 15:09:59 +0100

    Implement DAO for Task

---
+++ src/main/java/com/labgeek/DAO/ITaskDAO.java
@@ -0,0 +1,30 @@
+package com.labgeek.DAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.labgeek.models.Task;
+
+public interface ITaskDAO {
+
+	List<Task> findAll() throws SQLException;
+
+	List<Task> findByUser() throws SQLException;
+
+	List<Task> findByStatus() throws SQLException;
+
+	/** @return Generated ID of Task */
+
+	int insert(Task task) throws SQLException;
+
+	/** @return boolean if row was updated Task */
+
+	boolean update(Task task) throws SQLException;
+
+	/** @return boolean if row was removedd Task */
+
+	boolean delete(int id) throws SQLException;
+
+	long count() throws SQLException;
+
+}
