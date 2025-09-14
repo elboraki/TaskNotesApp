a876ca6a319b3a200897ac7e2ec8817d59c47356
elboraki <younes.elboraki@gmail.com>
2025-09-14 23:47:03 +0100

    Create add note view

--- src/main/java/com/labgeek/controller/NoteServlet.java
+++ src/main/java/com/labgeek/controller/NoteServlet.java
@@ -38,28 +38,36 @@ public class NoteServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		int page = 1;
-		int recordsPerPage = 10;
-		String search = request.getParameter("search"); // new
-		if (request.getParameter("page") != null) {
-			page = Integer.parseInt(request.getParameter("page"));
-		}
-		try {
-			List<Note> notes = noteDAO.getAll((page - 1) * recordsPerPage, recordsPerPage, search);
-			request.setAttribute("notes", notes);
-			int noOfRecords = noteDAO.count(search);
-			int noOfPages = (int) Math.ceil(noOfRecords * 1.0 / recordsPerPage);
-			request.setAttribute("noOfPages", noOfPages);
-			request.setAttribute("currentPage", page);
-			request.setAttribute("contentPage", "notes/notes.jsp");
-			request.setAttribute("search", search == null ? "" : search);
-
+		
+		if(request.getParameter("action")!=null && request.getParameter("action").toString().equals("new")) {
+			request.setAttribute("contentPage", "notes/addNote.jsp");
+			System.out.println("");
 			request.getRequestDispatcher("layout.jsp").forward(request, response);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}else {
+			// TODO Auto-generated method stub
+			int page = 1;
+			int recordsPerPage = 10;
+			String search = request.getParameter("search"); // new
+			if (request.getParameter("page") != null) {
+				page = Integer.parseInt(request.getParameter("page"));
+			}
+			try {
+				List<Note> notes = noteDAO.getAll((page - 1) * recordsPerPage, recordsPerPage, search);
+				request.setAttribute("notes", notes);
+				int noOfRecords = noteDAO.count(search);
+				int noOfPages = (int) Math.ceil(noOfRecords * 1.0 / recordsPerPage);
+				request.setAttribute("noOfPages", noOfPages);
+				request.setAttribute("currentPage", page);
+				request.setAttribute("contentPage", "notes/notes.jsp");
+				request.setAttribute("search", search == null ? "" : search);
+
+				request.getRequestDispatcher("layout.jsp").forward(request, response);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
 
 	}
 
---
+++ src/main/webapp/notes/addNote.jsp
@@ -0,0 +1,39 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+
+<%
+String ctx = request.getContextPath();
+%>
+<div class="topbar mt-2">
+	<h1>Add new note</h1>
+
+</div>
+<!-- Optional flash message -->
+<c:if test="${not empty message}">
+	<p
+		style="background: #e8f5e9; border: 1px solid #c8e6c9; padding: 10px; border-radius: 6px;">
+		${message}</p>
+</c:if>
+<div class="container mt-4">
+
+	<form class="row g-3 mb-3" method="post" action="<%=ctx%>/notes">
+
+		<div class="mb-3">
+			<label class="form-label">body</label>
+			<textarea class="form-control" name="description" rows="3"></textarea>
+		</div>
+		<div class="mb-3">
+			<label class="form-label">Category</label> <select class="form-select" name="status"
+				aria-label="Default select status">
+				<option value="Pending" selected="Pending">Pending</option>
+				<option value="In Progress">In Progress</option>
+				<option value="Completed">Completed</option>
+			</select>
+		</div>
+
+		<div class="col-auto">
+			<button type="submit" class="btn btn-primary mb-3">Create</button>
+		</div>
+	</form>
+</div>
